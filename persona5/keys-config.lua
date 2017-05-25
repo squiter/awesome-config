f2a5aa4fe472e775115e497a776f3bf53ba24457
Brunno dos Santos <squiter85@gmail.com>
2017-05-25 13:55:46 -0300

    Remove behaviour that mouse's wheel changes tag

--- persona5/keys-config.lua
+++ persona5/keys-config.lua
@@ -112,9 +112,7 @@ function hotkeys:init(args)
 	local mainmenu = args.menu
 
 	self.mouse.root = (awful.util.table.join(
-		awful.button({ }, 3, function () mainmenu:toggle() end),
-		awful.button({ }, 4, awful.tag.viewnext),
-		awful.button({ }, 5, awful.tag.viewprev)
+		awful.button({ }, 3, function () mainmenu:toggle() end)
 	))
 
 	-- Layouts
